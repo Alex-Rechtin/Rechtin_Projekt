4a7491d49559ce233708ea66e59ece7be88bfae3
Alex Rechtin <Alex.Rechtin@gmail.com>
2021-08-30 18:25:12 +0200

    FEEProjekt hinzugefügt

---
+++ Projekt/Rechtin/js/funktionen.js
@@ -0,0 +1,179 @@
+// maximal 100 und minimal 10 sind für die Summe 
+let max = 100; 
+let min = 10; 
+
+//Hier wird erst die summe brechnet und dann der summand1 danach den summand2! 
+// minussummand ist der minimal wert für summand1 
+let minsummand = 1; 
+
+// erstellt eine Zufallszahl zwischen 10-100
+function zufall(min,max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// Diese Funktion erstellt eine Aufgabe und return sie
+function neueAufgabe(){
+    let summe = zufall(min,max); 
+
+    //bei summand1 wird eine zufalls zahl zwischen 1 und der summe -1 
+    //--> damit summand1 nicht gleich summe sein kann! 
+    let summand1 = zufall(minsummand,summe - 1); 
+
+    // summand 2 = damit hier das richtige rauskommt wird summe -summand1 gerechnet 
+    let summand2 = summe - summand1; 
+
+    //array der Aufgabe die gestellt wird
+    let aufgabe = [summand1,summand2,summe]; 
+
+    return aufgabe;
+}
+
+
+
+// minimale wert  und maximale Wert für die Funktion zahl Anpassung
+let min1 = 1;
+let max1 = 99;
+/**
+ * Diese Funktion macht das wenn die Zahl kleiner als der minimale Wert ist, dass die zahl angepasst wird als min1 
+ * und größer als der maximale Wert ist wird die Zahl angepasst als max1
+ * am ende wird die angepasste Zahl zurückgegeben
+ * 
+ * @param {number} richtigeZahl die  Richtige Zahl der Aufgabe die übergeben wird 
+ * @param {number} erhoehungsZahl die Zahl die übergeben wird zur veränderung der Richtigenzahl für die andere Tür
+ * @returns die Angepasste Zahl
+ */
+function zahlAnpassung(richtigeZahl,erhoehungsZahl) {
+    if (richtigeZahl < min1){
+        richtigeZahl = min1;
+    }
+    else if (richtigeZahl > max1){
+        richtigeZahl = max1;
+    }
+    while (richtigeZahl === erhoehungsZahl) {
+        richtigeZahl = zufall(min1,max1);
+        return richtigeZahl;
+    }
+    return richtigeZahl;
+    
+}
+
+//Diese Funktion erstellt die Zahlen auf den Türen
+//entweder die linke oder rechte Tür ist dann richtig
+function setzeTueren(richtigeTuer) {
+    let zufallsTuer = zufall(0,1);
+    let zufallsZahl = zufall(-10,10);
+    if (zufallsZahl === 0) {
+        zufallsZahl++;  
+    }
+    if (zufallsTuer === 0) {
+        linkeTuer.textContent = richtigeTuer;
+        rechteTuer.textContent = zahlAnpassung(richtigeTuer + zufallsZahl);
+        linkeTuer.istRichtig = true;
+        rechteTuer.istRichtig = false;
+    } else {
+        rechteTuer.textContent = richtigeTuer;
+        linkeTuer.textContent = zahlAnpassung(richtigeTuer + zufallsZahl);
+        rechteTuer.istRichtig = true;
+        linkeTuer.istRichtig = false;
+    }
+}
+
+//Erstellt eine Zufallsaufgabe wo summand1,summand2 oder summe gesucht wird
+/**
+ * In der Funktion setzteAufgabe wird die aufgabe von der Funktion neueAufgabe genommen,
+ * aufgeteilt und dann in die richtigen value Felder gesetzt.
+ * Im Game ist es dann summand1 + summand2 = summe!!!
+ */
+function setzeAufgabe(){
+    let raus = zufall(0,2);
+    [summand1,summand2,summe] = neueAufgabe();
+    zahl1.value = summand1;
+    zahl2.value = summand2;
+    summenFeld.value = summe;
+    console.log(raus);
+    switch (raus) {
+        case 0:
+            zahl1.value = "?";
+            setzeTueren(summand1);
+            break;
+        case 1:
+            zahl2.value = "?";
+            setzeTueren(summand2);
+            break;
+        case 2:
+            summenFeld.value = "?";
+            setzeTueren(summe);
+            break;
+    }
+}
+
+/**
+ * Farbeetage ist ein Array mit mehreren Farben
+ * In der Funktion farbwechsel wird per Zufall eine Farbe des Arrays genommen und
+ * für die Stagezahl benutzt, pro Sekunde eine neue Farbe für die Stagezahl
+ * Am ende führt sich die Funktion selber aus --> ()
+ *  
+ */
+(function farbwechsel(){
+    let farbeetage = ['white','cyan','deepskyblue','yellow','magenta','lime','gold','orange'];
+    setInterval(function () {
+        let zufall = Math.floor (farbeetage.length * Math.random());
+        stagezahl.style.color = farbeetage[zufall];
+        
+    },1000);
+})();
+
+
+/**
+ *  funktion zum drucken der seite mit der Urkunde die man am ende bekommen soll
+ */ 
+function druck() { 
+        window.print(); 
+} 
+
+/**
+ *  Diese Funktion macht das im Hauptmenü oder am ende alles verschwinden,
+ *  was dort nicht gebraucht wird
+ */
+//lässt alles weg von der index html was man beim hauptmenü oder am ende nicht braucht
+function gehtWeg(){
+    spiel.style.display = "none";
+    aufgabe.style.display = "none";
+    divtext.style.display = "none";
+    linkeTuer.style.display= "none";
+    rechteTuer.style.display= "none";
+    hauptmenuebutton.style.display = "none";
+    neustartbutton.style.display = "none";
+    weiterbutton.style.display = "none"
+}
+
+/**
+ * Diese Funktion lässt die blockierten div-Container wieder auftauchen
+ */
+function kommtwieder(){
+    spiel.style.display = "block";
+    aufgabe.style.display = "block";
+    divtext.style.display = "block";
+    linkeTuer.style.display= "block";
+    rechteTuer.style.display= "block";
+}
+
+/**
+ * Diese Funktion lässt die Button wieder auftauchen 
+ */
+function buttonstrue(){
+    weiterbutton.style.display = "block";
+    neustartbutton.style.display = "block";
+    hauptmenuebutton.style.display = "block";
+      
+}
+
+/**
+ * Diese Funktion lässt die Butoons wieder verschwinden
+ */
+function buttonsfalse(){
+    weiterbutton.style.display = "none";
+    neustartbutton.style.display = "none";
+    hauptmenuebutton.style.display = "none";
+}
+
---
+++ Projekt/Rechtin/js/main.js
@@ -0,0 +1,164 @@
+const body1 = document.querySelector("body")
+
+// Die Aufgabe
+const summenFeld = document.querySelector("#summe");
+const zahl1 = document.querySelector("#sum1");
+const zahl2 = document.querySelector("#sum2");
+
+// Die div container
+const aufgabe = document.querySelector("#aufgabe");
+const spiel = document.querySelector("#spiel");
+//Der Text ob richtige Tür oder falsche Tür
+const divtext = document.querySelector("#text");
+
+// Die ID's von den Etagen
+const stage = document.querySelectorAll("#stage");
+const stagezahl = document.querySelector("#etagennummer");
+
+// Die Zahlen auf den Türen
+const linkeTuer = document.querySelector("#linkeTuer");
+const rechteTuer = document.querySelector("#rechteTuer");
+const alleTueren = document.querySelectorAll(".tuer");
+
+//Die Buttons 
+const startbutton = document.querySelector("#startbutton");
+const weiterbutton= document.querySelector("#weiter");
+const neustartbutton = document.querySelector("#neustart");
+const hauptmenuebutton = document.querySelector("#hauptmenue");
+
+console.log(divtext);
+
+setzeAufgabe();
+
+// Diese Variable ist da um die Etage anzugeben
+let etage = 1;
+
+// Diese Variable ist da um Die versuche hoch zu zählen 
+let fehlversuche = 0;
+
+// Diese Variablen sind da um den Background ändern zu lassen 
+let minbackground = 1;
+let maxbackground = 4;
+
+// Die Variable startgedruekt ist da um zu sagen ob man auf den startbutton gedrückt hat oder nicht 
+// Die Variable pausegedruekt ist da um zu sagen ob man auf die ESC Taste gedrückt hat oder nicht 
+let startgedruekt = false;
+let pausegedruekt = false;
+gehtWeg();
+// lässt beim drücken vom startbutton die methode kommtwieder anwenden und denn startbutton verschwinden
+startbutton.addEventListener("click", (e) =>{
+    kommtwieder();
+    startbutton.style.display = "none";
+    // startgedruekt wird true weil das der startbutton gedrückt wurde
+    startgedruekt = true;
+    stagezahl.textContent = etage;
+});
+
+// macht das man die esc taste benutzen kann um auf die buttons zu drücken
+// und wieder um die button wieder verschwinden zu lassen
+body1.addEventListener ("keydown", (evt) => {
+	if (evt.key === "Escape" && startgedruekt) {
+        if (!pausegedruekt){
+            console.log ("Bingo!");
+            buttonstrue();
+            pausegedruekt = true;
+        }
+        else{
+            buttonsfalse();
+            pausegedruekt = false;
+        }
+	}
+});
+
+weiterbutton.addEventListener("click", (e) =>{
+    buttonsfalse();
+    pausegedruekt = false;
+});
+
+
+// Beim Drücken des hauptmenuebutton wird die Seite neu reloaded
+hauptmenuebutton.addEventListener("click", (e) =>{
+    //reload der Seite
+    location.reload();
+});
+
+/**
+ * Beim Drücken des hauptmenuebutton wird die Seite neu reloaded
+ * Die etage wird zurück auf 1 gesetzt und die fehlversuche auf 0;
+ * der divtext wird entfernt
+ */
+neustartbutton.addEventListener("click", (e) =>{
+    pausegedruekt = false;
+    buttonsfalse();
+    setzeAufgabe();
+    etage = 1;
+    fehlversuche = 0;
+    stagezahl.textContent = etage;
+    console.log(fehlversuche + " Fehlersuche");
+    divtext.textContent = "";
+    body1.style.backgroundImage = ("url('pics/background1.png")
+});
+
+
+
+
+/**
+ * Wird ausgeführt beim Klicken der Türen im Game
+ */
+for (let index = 0; index < alleTueren.length; index++) {
+    alleTueren[index].addEventListener("click", (e) =>{
+        console.log(e.target);
+        // wenn es pausiert return es und hört auf 
+        if (pausegedruekt) {
+            return;
+        }
+        // e.target darin wird der button gespeichert auf den gedrückt wurde (<button id="linke/rechte-Tuer" class="tuer">)
+        if (e.target.istRichtig === true) {
+            //Bei Stufe 30 ist das Ziel des Spiels erreicht und
+            // die Zahlen auf den Türen verschwinden
+            if (etage == 30) {
+                gehtWeg();
+                body1.style.backgroundImage ="url('pics/turmraum.png')";
+            }
+            else{
+            console.log("richtig");
+            // farbe 
+            divtext.style.color = "green";
+            divtext.textContent = "Korrekt";
+            // etage wird erhöht
+            etage++;
+            //zufälliger Background wird genommen aus dem Ordner pics
+            body1.style.backgroundImage = "url('pics/background"+ zufall(minbackground,maxbackground) + ".png')";
+            }
+        }
+        else{
+            //Bei Etage 10 und Höher fällt der Spieler um 5 Etagen runter 
+            if (etage >= 10 ) {
+                etage-=5;
+                //Fehlversuch erhöhen
+                fehlversuche++;
+            }
+            //Bei Etage höher als 1 fällt der Spieler ein Etage
+            else if (etage > 1) {
+                etage--;
+                //Fehlversuch erhöhen
+                fehlversuche++;
+            }
+            divtext.textContent = "Du bist gefallen!";
+            divtext.style.color = "red";
+        }
+    
+        setzeAufgabe();
+        stagezahl.textContent = etage;
+        console.log(fehlversuche + " Versuche");
+    });
+    /*
+    window.addEventListener('afterprint', (event) => {
+        console.log('After print');
+        //reload der Seite
+        location.reload();
+    });*/
+}
+
+//Ab 30 kriegt der spieler die möglichkeit von 2 türen: weiter zugehen oder das spiel ab der Stufe zu beenden!!!
+//parameter,funktion,variablen
